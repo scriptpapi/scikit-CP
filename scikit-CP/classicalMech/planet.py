a2522a954b0d0051ee82cae9363976142878dba7
Nawaf Abdullah <nawaf97k@gmail.com>
2018-10-18 15:53:03 +0800

    changed method names

--- scikit-CP/classicalMech/planet.py
+++ scikit-CP/classicalMech/planet.py
@@ -3,6 +3,7 @@
 #   Creation Date: 9/October/2018
 #   Description: 2D numerical modeling of a planet as an N-body
 from math import sqrt
+import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
@@ -12,92 +13,101 @@ PI = 3.14159265359
 
 
 class Planet:
-    def __init__(self, i_x, i_y, i_m, i_dt, i_vx=0, i_vy=0, i_key=None):
+    def __init__(self, i_m, x_i, y_i, vx_i=0, vy_i=0, i_key=None):
         """
-        :param i_x: initial x location
-        :param i_y: initial y location
-        :param i_m: mass of the planet
-        :param i_dt: calculation time step size
-        :param i_vx: initial velocity in the x-direction
-        :param i_vy: initial velocity in the y=direction
-        :param i_key: a key to be assigned to the object to make identifying it
+        :param i_m: mass
+        :param x_i: initial x position
+        :param y_i:  initial y position
+        :param vx_i: initial x velocity
+        :param vy_i: initial y velocity
+        :param i_key: key string that can be used identify the object in a list or a tree
         """
-        self.dx = [i_x]
-        self.dy = [i_y]
         self.m = i_m
-        self.vx = [i_vx]
-        self.vy = [i_vy]
-        self.dt = i_dt
+        self.dx = [x_i]
+        self.dy = [y_i]
+        self.vx = [vx_i]
+        self.vy = [vy_i]
         self.key = i_key
 
-    def single_orbit(self, num_steps):
+    def orbit(self, n, dt):
         """
-        Calculates the trajectory of the planet without the effect of any other celestial object.
-        :param num_steps: Number of time steps
+        Calculates planet's orbit around a star without the influence of other planets
+        :param n: number of time steps
+        :param dt: time step size
         :return: x and y trajectory lists
         """
-        for i in range(num_steps):
-            ri = sqrt(self.dx[i] ** 2 + self.dy[i] ** 2)
-            self.vx.append(self.vx[i] - (4 * PI * PI * self.dx[i] * self.dt / ri ** 3))
-            self.vy.append(self.vy[i] - (4 * PI * PI * self.dy[i] * self.dt / ri ** 3))
-            self.dx.append(self.dx[i] + self.vx[i + 1] * self.dt)
-            self.dy.append(self.dy[i] + self.vy[i + 1] * self.dt)
+        for i in range(n):
+            ri = self.R_i(i)
+            self.vx.append(self.vx[i] - (4 * PI * PI * self.dx[i] * dt / ri ** 3))
+            self.vy.append(self.vy[i] - (4 * PI * PI * self.dy[i] * dt / ri ** 3))
+            self.dx.append(self.dx[i] + self.vx[i + 1] * dt)
+            self.dy.append(self.dy[i] + self.vy[i + 1] * dt)
         return self.dx, self.dy
 
     def r_i(self, i):
         """
-        :param: the number of the current time step point
-        :return: distance from the center of the system
+        :param i: time step point
+        :return: position vector
         """
-        r_i = sqrt((self.dx[i]**2) + (self.dy[i]**2))
+        return np.array([self.dx[i], self.dy[i]])
+
+    def R_i(self, i):
+        """
+        :param i: time step point
+        :return: magnitude of position vector at i
+        """
+        r_i = sqrt((self.dx[i] ** 2) + (self.dy[i] ** 2))
         return r_i
 
     def r_ab_i(self, i, planet_b):
+        """
+        :param i: time step point
+        :param planet_b: The other planet which the distance is being calculated from
+        :return: The position vector from planet a to planet b
+        """
+        a = np.array([self.dx[i], self.dy[i]])
+        b = np.array([planet_b.dx[i], planet_b.dy[i]])
+        ab = a - b
+        return ab
+
+    def R_ab_i(self, i, planet_b):
         """
         :param i: the number of the current time step point
         :param planet_b: The other planet which the distance is being calculated from
         :return: distance of both objects from the center of the system.
         """
-        x_temp = (self.dx[i] - planet_b.dx[i])**2
-        y_temp = (self.dy[i] - planet_b.dy[i])**2
+        x_temp = (self.dx[i] - planet_b.dx[i]) ** 2
+        y_temp = (self.dy[i] - planet_b.dy[i]) ** 2
         r_ab_i = sqrt(x_temp + y_temp)
         return r_ab_i
 
-    def init_params(self, i_x, i_y, i_m, i_dt, i_vx=0, i_vy=0):
+    def set_params(self, i_m=None, x_i=None, y_i=None, vx_i=None, vy_i=None):
         """
-        Changes the parameters for the planet object
+        Set all physical parameters of the planet object
         """
-        self.dx = [i_x]
-        self.dy = [i_y]
-        self.m = i_m
-        self.vx = [i_vx]
-        self.vy = [i_vy]
-        self.dt = i_dt
-
-    def set_mass(self, i_m):
-        """
-        :param i_m: new mass of Planet object
-        """
-        self.m = i_m
-
-    def set_init_coordinates(self, x_i, y_i):
-        """
-        Set projectiles initial coordinates.
-        :param x_i: initial x coordinates
-        :param y_i: initial y coordinates
-        """
-        self.dx = [x_i]
-        self.dy = [y_i]
+        if i_m is not None:
+            self.m = i_m
+        if x_i is not None:
+            self.dx = [x_i]
+        if y_i is not None:
+            self.dy = [y_i]
+        if vx_i is not None:
+            self.vx = [vx_i]
+        if vy_i is not None:
+            self.vy = [vy_i]
 
     def set_key(self, i_key):
         """
-        :param i_key: set a key for the projectile object to be found later.
+        :param i_key: set key identifying object. must be a string.
         """
-        self.key = i_key
+        if i_key is isinstance(i_key, str):
+            self.key = i_key
+        else:
+            raise ValueError("Key value must be a string.")
 
     def get_key(self):
         """
-        :return: object's key
+        :return: returns planets key
         """
         return self.key
 
