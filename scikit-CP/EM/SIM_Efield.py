110e691bd882dda065af1968618f890f059aa23a
Nawaf Abdullah <nawaf97k@gmail.com>
2018-10-26 15:38:30 +0800

    potential

--- scikit-CP/EM/SIM_Efield.py
+++ scikit-CP/EM/SIM_Efield.py
@@ -3,7 +3,8 @@
 #   Creation Date: 25/October/2018
 #   Description: Numerical simulation script of electric field and potential
 import numpy as np
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class EField:
     def __init__(self, i_size):
@@ -21,10 +22,13 @@ class EField:
         """
         V = self.V_2d
         for n in range(num_iter):
-            for i in range(self.dim):
-                for j in range(self.dim):
-                    V[i][j] = (V[i+1][j] + V[i-1][j] +
-                               V[i][j+1] + V[i][j-1]) * (1/4)
+            for i in range(1, self.dim-1):
+                for j in range(1, self.dim-1):
+                    if V[i][j] == 1 or V[i][j] == -1:
+                        continue
+                    else:
+                        V[i][j] = (V[i + 1][j] + V[i - 1][j] +
+                                   V[i][j + 1] + V[i][j - 1]) * (1 / 4)
         self.V_2d = V
         return self.V_2d
 
@@ -33,7 +37,6 @@ class EField:
         calculates the electric field on surface
         :param num_iter: number of iterations of the numerical method calculations
         """
-
 
     def space_potential(self, num_iter):
         """
@@ -60,7 +63,11 @@ class EField:
     def set_capacitor_lines(self):
         """
         sets the location and the voltage of the capacitor lines on 2d surface.
+        :param xi: position of the capacitor line on the x-axis
+        :param Vi: voltage of the capacitor line
         """
+        self.V_2d[40:42, 10:90] = 1
+        self.V_2d[60:62, 10:90] = -1
 
     def set_capacitor_plates(self):
         """
@@ -76,3 +83,13 @@ class EField:
         """
         plots the electric field lines.
         """
+        fig, ax0 = plt.subplots(nrows=1)
+        im = ax0.pcolormesh(self.V_2d)
+        fig.colorbar(im, ax=ax0)
+        plt.show()
+
+
+aha = EField(100)
+aha.set_capacitor_lines()
+aha.surface_potential(500)
+aha.plot_efield()
